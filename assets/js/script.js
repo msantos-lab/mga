ab4ac760de51a410a2f0124025d985da5e48d399
msantos-lb <msantosdev.lab@gmail.com>
2023-06-04 18:18:53 +0100

    ajustes finais

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,32 +2,26 @@
 let btn_content = document.querySelector(".btn-content");
 let btn_icon = document.querySelector(".btn-icon");
 
-function bgColorBtnOn(){
-  btn_content.classList.toggle("bg-bronze");
-  btn_icon.classList.toggle("bg-black");
+function bgColorBtnOn() {
+    btn_content.classList.toggle("bg-bronze");
+    btn_icon.classList.toggle("bg-black");
 };
 
-function bgColorBtnOut(){
-  btn_content.classList.remove("bg-bronze");
-  btn_icon.classList.remove("bg-black");
- };
+function bgColorBtnOut() {
+    btn_content.classList.remove("bg-bronze");
+    btn_icon.classList.remove("bg-black");
+};
 
 
- let menu_path = window.location.pathname.replace("/", "").replace(".html", "");
- let menu = document.querySelectorAll(".menu li");
-//  console.log(menu_path.match("exposicoes")[0]);
+let menu_path = window.location.pathname.replace("/", "").replace(".html", "");
+let menu = document.querySelectorAll(".menu li");
 
- menu.forEach((item) =>{
-  console.log(item);
-
+menu.forEach((item) => {
   let limpa_item = item.innerText.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '')
-
-  console.log("match " + menu_path.match(limpa_item));
-
-  if(limpa_item == menu_path || menu_path.match(limpa_item)){
+  if (limpa_item == menu_path || menu_path.match(limpa_item)) {
     item.id = "menu-ativo";
   }
- });
+});
 
 //              MENU
 
@@ -35,7 +29,7 @@ const btnMenu = document.querySelector('[data-menu="button"]');
 const menuMob = document.querySelector('[data-menu="list"]');
 console.log(menuMob)
 
-function ativaMenu(){
+function ativaMenu() {
   menuMob.classList.toggle("menuMobOn");
 }
 
